55fd99920f4b27a1c10114f3777e89cd681fd20d
amin_mehr <amin.mehravar@yahoo.com>
2025-06-07 16:59:28 +0300

    fix: added missing key prop to list items in ProjectsSection

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,7 +4,6 @@ import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-import grainImage from "@/assets/images/grain.jpg";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
 
@@ -51,17 +50,19 @@ export const ProjectsSection = () => {
   return (
     <section id="projects" className="pb-16 lg:py-24">
       <div className="container">
-        <SectionHeader eyebrow="حرفه ای و دوستانه"
+        <SectionHeader
+          eyebrow="حرفه‌ای و دوستانه"
           title="پروژه‌های منتخب"
-          description="از ایده تا تجربه‌ای جذاب" />
+          description="از ایده تا تجربه‌ای جذاب"
+        />
 
         <div className="flex md:mt-20 flex-col mt-10 gap-20">
           {portfolioProjects.map((project, projectIndex) => (
             <Card
               key={project.title}
-              className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky "
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
               style={{
-                top: `calc(64px + ${projectIndex * 40}px`,
+                top: `calc(64px + ${projectIndex * 40}px)`,
               }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
@@ -71,26 +72,34 @@ export const ProjectsSection = () => {
                     <span>&bull;</span>
                     <span>{project.year}</span>
                   </div>
-                  <h3 className="text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+                  <h3 className="text-2xl mt-2 md:mt-5 md:text-4xl">
+                    {project.title}
+                  </h3>
                   <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">{project.results.map((result) => (
-                    <li className="flex gap-2 text-sm md:text-base text-white/60">
-                      <CheckCircleIcon className="size-5 md:size-6" />
-                      <span>{result.title}</span>
-                    </li>
-                  ))}</ul>
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm md:text-base text-white/60"
+                      >
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
                   <a href={project.link}>
-                    <button className=
-                      "bg-white text-gray-900 h-12 w-full md:w-auto px-6 rounded-xl inline-flex items-center justify-center gap-2 mt-8" >
+                    <button className="bg-white text-gray-900 h-12 w-full md:w-auto px-6 rounded-xl inline-flex items-center justify-center gap-2 mt-8">
                       <span>مشاهده</span>
-                      <ArrowUpRightIcon className='size-3' />
-
+                      <ArrowUpRightIcon className="size-3" />
                     </button>
                   </a>
                 </div>
                 <div className="relative">
-                  <Image src={project.image} alt={project.title}
-                    className="mt-8 -mb-4 mx-auto md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" />
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 mx-auto md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
                 </div>
               </div>
             </Card>
